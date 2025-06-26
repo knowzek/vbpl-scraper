518526e4c056a5388b43fba77507a8d14d3b2592
knowzek <71522616+knowzek@users.noreply.github.com>
2025-06-26 11:04:21 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 def get_library_config(library):
-    """Return config dict for given library code."""
     if library == "vbpl":
         return {
             "spreadsheet_name": "Virginia Beach Library Events",
@@ -7,6 +6,9 @@ def get_library_config(library):
             "log_worksheet_name": "VBPL Log",
             "organizer_name": "Virginia Beach Public Library",
             "event_name_suffix": " (Virginia Beach)",
+            "drive_folder_id": "16pkTscctVQgywEU8mkHeqE3JS55dnlrz",
+            "email_recipient": "knowzek@gmail.com",
+            "email_subject": "New VBPL CSV export ready"
         }
     elif library == "npl":
         return {
@@ -15,6 +17,9 @@ def get_library_config(library):
             "log_worksheet_name": "NPL Log",
             "organizer_name": "Norfolk Public Library",
             "event_name_suffix": " (Norfolk)",
+            "drive_folder_id": "1pr1xTMnxE9rAkhmG6YxnSkZGWb_7vkpn",
+            "email_recipient": "knowzek@gmail.com",
+            "email_subject": "New NPL CSV export ready"
         }
     else:
         raise ValueError(f"Unsupported library code: {library}")
