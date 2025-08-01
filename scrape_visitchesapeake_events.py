14c1a19460b401864bf995942c82473daa4df4bd
knowzek <71522616+knowzek@users.noreply.github.com>
2025-07-31 20:17:14 -0700

    Update scrape_visitchesapeake_events.py

--- scrape_visitchesapeake_events.py
+++ scrape_visitchesapeake_events.py
@@ -1,4 +1,4 @@
-    from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 import re
