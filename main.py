718b5d43fcbdd1a14ef6af69230bbd4ec528491e
knowzek <71522616+knowzek@users.noreply.github.com>
2025-08-27 22:44:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -38,6 +38,9 @@ if __name__ == "__main__":
 
     if library == "vbpl":
         events = scrape_vbpl_events(cutoff_date=cutoff)
+
+    if library == "poquosonpl":
+        events = scrape_poquosonpl_events(cutoff_date=cutoff)
     
     elif library == "npl":
         events = scrape_npl_events(mode=mode)
