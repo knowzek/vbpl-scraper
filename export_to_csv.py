69b867e52f0993a58373579cbc67289a6bd65f9e
knowzek <71522616+knowzek@users.noreply.github.com>
2025-08-28 10:56:50 -0700

    Update export_to_csv.py

--- export_to_csv.py
+++ export_to_csv.py
@@ -473,7 +473,7 @@ def export_events_to_csv(library="vbpl", return_df=False):
     return csv_path
 
 if __name__ == "__main__":
-    LIBRARIES = ["vbpl", "npl", "chpl", "nnpl", "hpl", "ppl", "spl", "vbpr", "visithampton", "visitchesapeake", "ypl", "visitnewportnews", "portsvaevents", "visitsuffolk", "visitnorfolk", "poquosonpl"]
+    LIBRARIES = ["vbpl", "npl", "chpl", "nnpl", "hpl", "ppl", "spl", "vbpr", "visithampton", "visitchesapeake", "ypl", "visitnewportnews", "portsvaevents", "visitsuffolk", "visitnorfolk", "poquosonpl", "visityorktown"]
     print("🧪 Running unified CSV export for LIBRARIES:", LIBRARIES)
 
     all_exports = []
