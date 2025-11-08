266b4aacaee72d8fe2c0930338b3a1bb4eb6622c
knowzek <71522616+knowzek@users.noreply.github.com>
2025-11-07 17:39:57 -0800

    Update scrap_visithampton_events.py

--- scrap_visithampton_events.py
+++ scrap_visithampton_events.py
@@ -35,8 +35,8 @@ def clean_data(full_data):
             elif isinstance(t, str):
                 tags.append(t.strip())
 
-        #if not "Things to Do with Kids" in tags:
-        #    continue
+        if not "Things to Do with Kids" in tags:
+            continue
 
         for keyword, categorie in TITLE_KEYWORD_TO_CATEGORY_RAW.items():
             if check_keyword(keyword.lower(), event['Event Name'].lower()) or check_keyword(keyword, event['Event Description'].lower()):
